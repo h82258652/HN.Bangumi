ab821f64549c57192bdedb891cb27f49f0da8665
h82258652 <h82258652@hotmail.com>
2019-04-15 16:28:57 +0800

    添加转换器

---
+++ src/HN.Bangumi.Uwp.17763/Converters/IsNullOrEmptyStringConverter.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Data;
+
+namespace HN.Bangumi.Uwp.Converters
+{
+    public class IsNullOrEmptyStringConverter : IValueConverter
+    {
+        public bool IsInversed { get; set; }
+
+        public object Convert(object value, Type targetType, object parameter, string language)
+        {
+            var val = string.IsNullOrEmpty((string)value);
+            if (IsInversed)
+            {
+                return val ? Visibility.Collapsed : Visibility.Visible;
+            }
+            else
+            {
+                return val ? Visibility.Visible : Visibility.Collapsed;
+            }
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, string language)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
