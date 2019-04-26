fbbeca544469b234c78002414ce5d5c67a6c8844
h82258652 <h82258652@hotmail.com>
2019-04-26 10:25:57 +0800

    添加connected animation扩展

---
+++ src/HN.Bangumi.Uwp.17763/Extensions/ConnectedAnimationExtensions.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Runtime.CompilerServices;
+using Windows.UI.Xaml.Media.Animation;
+
+namespace HN.Bangumi.Uwp.Extensions
+{
+    public static class ConnectedAnimationExtensions
+    {
+        private static readonly ConditionalWeakTable<ConnectedAnimation, Dictionary<string, object>> WeakTable = new ConditionalWeakTable<ConnectedAnimation, Dictionary<string, object>>();
+
+        public static object GetExtraData(this ConnectedAnimation animation, string key)
+        {
+            if (animation == null)
+            {
+                throw new ArgumentNullException(nameof(animation));
+            }
+            if (key == null)
+            {
+                throw new ArgumentNullException(nameof(key));
+            }
+
+            if (WeakTable.TryGetValue(animation, out var dictionary))
+            {
+                if (dictionary.TryGetValue(key, out var value))
+                {
+                    return value;
+                }
+            }
+
+            return null;
+        }
+
+        public static void SetExtraData(this ConnectedAnimation animation, string key, object value)
+        {
+            if (animation == null)
+            {
+                throw new ArgumentNullException(nameof(animation));
+            }
+            if (key == null)
+            {
+                throw new ArgumentNullException(nameof(key));
+            }
+
+            var dictionary = WeakTable.GetOrCreateValue(animation);
+            dictionary[key] = value;
+        }
+    }
+}
