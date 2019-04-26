2e2be3d1be12d4d05ddc154449adce4bc140573a
h82258652 <h82258652@hotmail.com>
2019-04-26 11:13:23 +0800

    添加转换器

---
+++ src/HN.Bangumi.Uwp.17763/Converters/IsEmptyCollectionToVisibilityConverter.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections;
+using System.Linq;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Data;
+
+namespace HN.Bangumi.Uwp.Converters
+{
+    public class IsEmptyCollectionToVisibilityConverter : IValueConverter
+    {
+        public bool IsInversed { get; set; }
+
+        public object Convert(object value, Type targetType, object parameter, string language)
+        {
+            var collection = (value as IEnumerable)?.Cast<object>();
+            if (collection?.Any() == true)
+            {
+                return IsInversed ? Visibility.Visible : Visibility.Collapsed;
+            }
+            else
+            {
+                return IsInversed ? Visibility.Collapsed : Visibility.Visible;
+            }
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, string language)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
